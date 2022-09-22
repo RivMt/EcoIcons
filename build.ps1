1c7a6bcf3c8d5bc84ce3a5d6a5a243a649723000
San Kang <devrivmt@gmail.com>
2022-09-22 22:31:12 +0900

    Add auto convert script for windows

---
+++ build.ps1
@@ -0,0 +1,16 @@
+# Make 'inkscape' as alias
+Set-Alias inkscape 'C:\Program Files\Inkscape\bin\inkscape.exe'
+
+# Set output size
+$outputSize = 512
+
+# Convert res/*.svg files to out/*.png
+echo "Begin conversion..."
+echo ""
+Get-ChildItem -File res | Foreach {
+    $outputName = $_.name.replace($_.extension, ".png")
+    inkscape -w $outputSize -h $outputSize res\$_.name -o $outputName
+    echo "$_ -> $outputName"
+}
+echo "Conversion end"
+echo ""
