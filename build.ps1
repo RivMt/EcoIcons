a947eb96fb3b6a2a1d9cf852687fa9dc4f9c2231
San Kang <devrivmt@gmail.com>
2022-09-26 20:49:26 +0900

    Fix script typo

--- build.ps1
+++ build.ps1
@@ -9,8 +9,9 @@ echo "Begin conversion..."
 echo ""
 Get-ChildItem -File res | Foreach {
     $outputName = $_.name.replace($_.extension, ".png")
-    inkscape -w $outputSize -h $outputSize .\res\$_ -o .\out\$outputName --export-overwriteW
+    inkscape -w $outputSize -h $outputSize .\res\$_ -o .\out\$outputName --export-overwrite
     echo "$_ -> $outputName"
 }
 echo "Conversion end"
 echo ""
+pause
